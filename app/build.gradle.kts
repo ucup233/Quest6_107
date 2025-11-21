284742eef56c564b12c70387082ecb7423cf7b0e
ucup233 <sufhidayat9@gmail.com>
2025-11-21 09:52:06 +0700

    Penambahan library dan dependency navigation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,7 +49,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
-
+    implementation(libs.androidx.navigation.runtime.ktx)
     implementation(libs.androidx.navigation.compose)
 
     testImplementation(libs.junit)
