5bd4b238c93279745853ea73f5a8fe3ed0bf1b49
ucup233 <sufhidayat9@gmail.com>
2025-11-21 09:59:59 +0700

    Penambahan library viewmodel

--- app/src/main/java/com/example/week8/view/Formulir.kt
+++ app/src/main/java/com/example/week8/view/Formulir.kt
@@ -25,7 +25,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
-
+import com.example.week8.R
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun FormIsian (
